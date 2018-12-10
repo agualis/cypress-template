d7660130aab18fec4a20cd610be962388c3f4710
Alberto Gualis <agualis@gmail.com>
2018-12-10 17:41:53 +0100

    first asesora feature

--- cypress/common/helpers.js
+++ cypress/common/helpers.js
@@ -3,12 +3,9 @@ export const ENTER = '{enter}'
 export const DOWN_ARROW = '{downarrow}'
 export const BACKSPACE = '{backspace}'
 
-// We need to create a new OPTIONS object
-// because Cypress appears to modify it in each execution
 export const OPTIONS = ()=> ({force: true})
 
 export const click = (id) => get(id).click(OPTIONS())
-export const clickWhenEnabled = (id) => get(id).click({force: false})
 
 export const clickFirst = (id) => get(id).first().click()
 
@@ -16,41 +13,19 @@ export const clickLast = (id) => get(id).last().click()
 
 export const uncheck = (id) => get(id).uncheck()
 
-export const singleSelect = (id, optionName) => get(id).select(optionName)
-
-export const write = (id, text) => get(id).clear(OPTIONS()).type(text, OPTIONS())
-
 export const get = (id) => cy.get(`[data-test=${id}]`)
 
 export const getInputByName = (name) => cy.get(`input[name="${name}"]`)
 
 export const getSelectByName = (name) => cy.get(`select[name="${name}"]`)
 
+export const fillInputWithName = (name, value) => getInputByName(name).type(value)
+
 export const fillInput = (name, value) => getInputByName(name).type(value)
 
-export const getSlow = (id) => cy.get(`[data-test=${id}]`, { timeout: 10000 })
-
-export const getFirst = (id) => get(id).first()
-
-export const assertValue = (id, value) => get(id).should('have.value', value)
-
-export const assertVisible = (id) => get(id).should('be.visible')
-
-export const assertNotVisible = (id) => get(id).should('not.be.visible')
-
-export const assertHasClass = (id, className) => get(id).should('have.class', className)
-
-export const assertNotHasClass = (id, className) => get(id).should('not.have.class', className)
-
-export const assertAtLeastOne = (id) => get(id).should((items)=> expect(items.size()).to.be.above(0))
-
-export const assertAtLeastOneWithClass = (className) => cy.get(className).should((items)=> expect(items.size()).to.be.above(0))
-
-export const assertOccurrences = (id, occurrenceNumber) => get(id).should((items)=> expect(items.size()).to.eql(occurrenceNumber))
-
-export const loginAndGoToHome = ()=> {
-  cy.login()
-  cy.visit('/')
+export const selectByClass= (className, value) => {
+    cy.get(className).click()
+    cy.contains(value).click()
 }
 
 export const urlContains = (urlPart) => cy.url().should('contain', urlPart)
@@ -58,19 +33,5 @@ export const urlContains = (urlPart) => cy.url().should('contain', urlPart)
 export const stubRequest = (url, alias) => {
   cy.server()
   cy.route(url, [{}]).as(alias)
-}
-
-export const getRandomFloat = () => {
-  const max = 2000
-  const min = 1000
-  //Force decimals to be .25 to avoid problems wit trailing zeroes
-  return (Math.floor(Math.random() * (max - min) + min) + 0.25).toFixed(2)
-}
-
-export const waitForGet = (command) => {
-  cy.server()
-  cy.route('GET', 'www.google.es' + '/async').as('get')
-  command()
-  cy.wait('@get')
 }
 
